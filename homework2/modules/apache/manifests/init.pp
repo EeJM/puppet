d1da99a02701f2b68f69ad18bbd2c4c1f62e5f6a
Eero Mikkonen <eero.mikkonen@hotmail.com>
2017-04-10 01:54:09 +0300

    Move virtualhost under /var/www/
    Selinux permissions do not like websites to be under home folders. The part where you set the virtual host to be in a user directory is still there but commented out but to make it work one has to fix selinux permissions on centos 7 and later.

--- homework2/modules/apache/manifests/init.pp
+++ homework2/modules/apache/manifests/init.pp
@@ -2,44 +2,60 @@ class apache {
 
 	if $operatingsystem == 'CentOS' {
 		package {'httpd':
-			ensure	=> 'latest',
+			ensure		=> 'latest',
 			allow_virtual	=> 'true',
 		}
 	}
 	else {
 		package {'apache2':
-			ensure	=> 'latest',
+			ensure		=> 'latest',
+			allow_virtual	=> 'true',
 		}
 	}
 
-	user { 'eero':
-		ensure	=> 'present',
-		home	=> '/home/eero',
-		# The password is hashed and since this is a test script the password is: bJyTK6!K6M6-Ae!KD
-		password => '$6$Gx6ZKz0m$v9NyqeDpTb/7w025j0DF80gNg0v.yZmLf8TtoI7BcqkH7Ij8spj9tkBSSQKGMrWjwmAkUUy39k6n..uLUoSX40',
-	}
-
-	file { '/home/eero/':
+	file { ['/var/www/',
+		'/var/www/eero.testi.com/', ]:
 		ensure	=> 'directory',
-		mode	=> '0751',
-		require => User['eero'],
-	}
-
-	file { '/home/eero/public_html/':
-		ensure	=> 'directory',
-		owner	=> 'eero',
-		group	=> 'eero',
 		mode	=> '0755',
-		require	=> User['eero'],
+		
 	}
 
-	file { '/home/eero/public_html/index.html':
-		owner	=> 'eero',
-		group	=> 'eero',
-		content	=> 'This is the working virtualhost for Eero.',
+	file { '/var/www/eero.testi.com/index.html':
+		ensure	=> 'present',
 		mode	=> '0644',
-		require	=> User['eero'],
+		content => 'This is the working virtualhost.',
 	}
+
+	# Use this if you want the virtual host inside of a users home folder.
+
+	#user { 'eero':
+	#	ensure	=> 'present',
+	#	home	=> '/home/eero',
+	#	# The password is hashed and since this is a test script the password is: bJyTK6!K6M6-Ae!KD
+	#	password => '$6$Gx6ZKz0m$v9NyqeDpTb/7w025j0DF80gNg0v.yZmLf8TtoI7BcqkH7Ij8spj9tkBSSQKGMrWjwmAkUUy39k6n..uLUoSX40',
+	#}
+
+	#file { '/home/eero/':
+	#	ensure	=> 'directory',
+	#	mode	=> '0755',
+	#	require => User['eero'],
+	#}
+
+	#file { '/home/eero/public_html/':
+	#	ensure	=> 'directory',
+	#	owner	=> 'eero',
+	#	group	=> 'eero',
+	#	mode	=> '0755',
+	#	require	=> User['eero'],
+	#}
+
+	#file { '/home/eero/public_html/index.html':
+	#	owner	=> 'eero',
+	#	group	=> 'eero',
+	#	content	=> 'This is the working virtualhost for Eero.',
+	#	mode	=> '0644',
+	#	require	=> User['eero'],
+	#}
 
 	if $operatingsystem == 'CentOS' {
 
@@ -60,9 +76,18 @@ class apache {
 		}
 	}
 
-	file { '/var/www/html/index.html':
-		content => 'This is the default landing page.',
-		require => Package['httpd'],
+	if $operatingsystem == 'CentOS' {
+
+		file { '/var/www/html/index.html':
+			content => 'This is the default landing page.',
+			require => Package['httpd'],
+		}
+	}
+	else {
+		file { '/var/www/html/index.html':
+                        content => 'This is the default landing page.',
+                        require => Package['apache2'],
+                }
 	}
 
 
